fd74fde95f8fa6ae1270d0fc91067827fdad0954
Nicolai Willems <niwil90@gmail.com>
2026-06-29 15:40:17 +0200

    feat: tilføj shopping lister

--- mad/build.sh
+++ mad/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir -p output
+mkdir -p output output_shopping
 
 files=$(find ./recipe-manual/ -name "*.md" -type f | sort)
 for file in $files; do
-    echo "Processing $file"
+    echo "processing $file"
     bb=$(basename $file)
     out_file="output/${bb%.md}.pdf"
     pandoc --template=./templates/recipe_template.tex -o $out_file $file
@@ -15,4 +15,22 @@ if [ -f "recipe_book.pdf" ]; then
     rm recipe_book.pdf
 fi
 
+echo "generating recipe_book.pdf from ${out_files}"
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -r150 -sOutputFile=recipe_book.pdf $out_files
+
+
+for file in $files; do
+    echo "processing $file"
+    bb=$(basename $file)
+    out_file="output_shopping/${bb%.md}.pdf"
+    pandoc --template=./templates/shopping_template.tex -o $out_file $file
+done
+
+out_files=$(find ./output_shopping/ -name "*.pdf" -type f | sort)
+if [ -f "shopping_book.pdf" ]; then
+    rm shopping_book.pdf
+fi
+
+echo "generating shopping_book.pdf from ${out_files}"
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -r150 -sOutputFile=shopping_book.pdf $out_files
+
